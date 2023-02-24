e9abf5bcbee10c77ad45c42a34f99bb59e918e08
Viktor Lidholt <viktorlidholt@gmail.com>
2023-02-24 13:06:33 +0100

    Managed zone for private dns

--- gcp_server/gcp/terraform/modules/serverpod/dns.tf
+++ gcp_server/gcp/terraform/modules/serverpod/dns.tf
@@ -1,3 +1,16 @@
+resource "google_dns_managed_zone" "private-dns" {
+  name        = "serverpod-${var.runmode}-private"
+  dns_name    = "private-${var.runmode}.${var.top_domain}."
+
+  visibility = "private"
+
+  private_visibility_config {
+    networks {
+      network_url = google_compute_network.serverpod.id
+    }
+  }
+}
+
 resource "google_dns_record_set" "api" {
   name         = "api.${var.top_domain}."
   managed_zone = "examplepod"
@@ -44,17 +57,4 @@ resource "google_dns_record_set" "redis-private" {
   type         = "A"
   ttl          = 60
   rrdatas      = [google_redis_instance.serverpod.host]
-}
-
-resource "google_dns_managed_zone" "private-dns" {
-  name        = "serverpod-${var.runmode}-private"
-  dns_name    = "private-${var.runmode}.${var.top_domain}."
-
-  visibility = "private"
-
-  private_visibility_config {
-    networks {
-      network_url = google_compute_network.serverpod.id
-    }
-  }
 }
